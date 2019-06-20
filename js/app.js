1035d8bc2fa357429c7dc827dc2d3bb40b883d3c
Brennan Martin <Brennan.Martin@arrow.com>
2019-06-20 11:18:39 -0600

    initial commit

--- js/app.js
+++ js/app.js
@@ -8,4 +8,47 @@
     "contBcg-3",
     "contBcg-4"
   ];
+
+  // set up counter
+  let counter = 0;
+
+  // select both buttons
+  const btn = document.querySelectorAll('.btn');
+
+  btn.forEach(function(btn){
+    btn.addEventListener('click', function(){
+
+      //get event target
+      let value = event.target;
+      // if statement for button left
+      if (value.classList.contains('btn-left')){
+        // move left
+        counter--;
+        // if counter is less than 0
+        if (counter < 0 ){
+          counter = pictures.length -1;
+        }
+        console.log(counter);
+        
+        document.querySelector('.img-container')
+          .style.backgroundImage = `url('img/${pictures[counter]}.jpeg')`;
+
+      } else if (value.classList.contains('btn-right')) {
+        //right
+        counter++;
+        // if counter is less than 0
+        if (counter > pictures.length -1 ){
+          counter = 0;
+        }
+        console.log(counter);
+        document.querySelector('.img-container')
+          .style.backgroundImage = `url('img/${pictures[counter]}.jpeg')`;
+
+      }
+    })
+  })
+  
+
+
 })();
+
